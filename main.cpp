c0df3e98e7e12e728a386e1c1717e960acb25b70
LizetteMendoza <lizette.mendoza6892@alumnos.udg.mx>
2021-08-14 01:15:16 -0500

    Prueba en programa principal de memoria dinamica

--- main.cpp
+++ main.cpp
@@ -7,19 +7,32 @@ using namespace std;
 
 int main() {
 	
-	/*Bodega b1= Bodega("Frituras",50,25.50);
+	Bodega b1= Bodega("Frituras",50,25.50);
 	Bodega b2;
 	b2.setProducto("Cerveza");
 	b2.setCantidad(25);
-	b2.setPrecio(32.50);*/
+	b2.setPrecio(32.50);
+	Bodega b4 = Bodega("Jugo",30,43.50);
+	Bodega b5 = Bodega("Agua",25,9.10);
 	/*cout<<b2.getProducto()<<endl;
 	cout<<b2.getCantidad()<<endl;
 	cout<<b2.getPrecio()<<endl;*/
+	
 
 	Matriz m;
+	m.insertar_final(b1);
+	m.insertar_final(b2);
+	m.insertar_final(b4);
+	m.insertar_inicio(b5);
 
-	/*m.agregarBodega(b2);
-	m.agregarBodega(b1);*/
+	cout<<"Tamanio"<<m.size()<<endl;
+	
+
+	for (size_t i = 0; i < m.size(); i++)
+	{
+		cout<<m[i]<<" ";
+	}
+	
 
 
 
@@ -31,10 +44,10 @@ int main() {
 	m<<b3;
 
 	m.mostrar();*/
-	cout<<"Recuperado"<<endl;
+	/*cout<<"Recuperado"<<endl;
 	m.recuperar();
 	
-	m.mostrar();
+	m.mostrar();*/
 
 	return 0;
 }
