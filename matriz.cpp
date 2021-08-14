8f63f3c150bab8ae278dc80d94c19255d149a2e5
LizetteMendoza <lizette.mendoza6892@alumnos.udg.mx>
2021-08-14 17:40:26 -0500

    Adaptacion de la clase administradora a templates

--- matriz.cpp
+++ matriz.cpp
@@ -3,58 +3,11 @@
 #include<string>
 
 using namespace std;
-Matriz::Matriz(){
 
-    arreglo = new Bodega[MAX];
-    cont=0;
-    tam = MAX;
-}
 
-Matriz::~Matriz(){
 
-    delete[] arreglo;
-}
-
-void Matriz::expandir(){
-    Bodega *nuevo = new Bodega[tam+MAX];
-
-    for (size_t i=0; i<cont; i++){
-        nuevo[i]=arreglo[i];
-    }
-
-    delete[] arreglo;
-    arreglo = nuevo;
-    tam=tam+MAX;
-}
-
-size_t Matriz::size(){
-    return cont;
-}
-
-void Matriz::insertar_final(Bodega &v){
-    if(cont == tam){
-        expandir();
-    }
-
-    arreglo[cont]=v;
-    cont++;
-}
-
-void Matriz::insertar_inicio(Bodega &v){
-    if(cont == tam){
-        expandir();
-    }
-
-    for (size_t i =cont; i > 0; i--)
-    {
-        arreglo[i]=arreglo[i-1];
-    }
-    arreglo[0] = v;
-    cont++;
-    
-}
-
-void Matriz::agregarBodega(const Bodega&b){
+template<class T>
+void Matriz<T>::agregarBodega(const T&b){
     if(cont<5){
         arreglo[cont] = b;
         cont++;
@@ -64,24 +17,9 @@ void Matriz::agregarBodega(const Bodega&b){
     }
 }
 
-void Matriz::mostrar(){
-    cout<<left;
-    cout<<setw(15)<<"Producto";
-    cout<<setw(10)<<"Cantidad";
-    cout<<setw(10)<<"Precio";
-    cout<<endl;
-    for(size_t i=0; i<cont; i++){
-        Bodega &b = arreglo[i];
-        /*cout<<"Producto: "<<b.getProducto()<<endl;
-        cout<<"Cantidad: "<<b.getCantidad()<<endl;
-        cout<<"Precio: "<<b.getPrecio()<<endl;*/
-        cout<<b;
-        //cout<<endl;
-    }
 
-}
-
-void Matriz::respaldar(){
+template<class T>
+void Matriz<T>::respaldar(){
 
     ofstream archivo("bodega.txt");
     if (archivo.is_open()){
@@ -96,7 +34,8 @@ void Matriz::respaldar(){
     archivo.close();
 }
 
-void Matriz::recuperar(){
+template<class T>
+void Matriz<T>::recuperar(){
 
     ifstream archivo("bodega.txt");
     if(archivo.is_open()){
--- matriz.h
+++ matriz.h
@@ -1,10 +1,15 @@
 #ifndef MATRIZ_H
 #define MATRIZ_H
 #include"clase.h"
+#include<fstream>
+#include<string>
+#include<iostream>
+#include<iomanip>
 
+template<class T>
 class Matriz{
 
-    Bodega *arreglo;
+    T *arreglo;
     size_t cont;
     size_t tam;
     const static size_t MAX = 5;
@@ -15,23 +20,100 @@ class Matriz{
 public:
     Matriz();
     ~Matriz();
-    void agregarBodega(const Bodega&b);
+    void agregarBodega(const T&b);
     void mostrar();
     void respaldar();
     void recuperar();
-    void insertar_final(Bodega &v);
-    void insertar_inicio(Bodega &v);
+    void insertar_final(T &v);
+    void insertar_inicio(T &v);
     size_t size();
-    Bodega operator[](size_t p){
+    T operator[](size_t p){
         return arreglo[p];
     }
 
-    friend Matriz& operator <<(Matriz& m, const Bodega&b){
+    friend Matriz& operator <<(Matriz& m, const T&b){
         m.agregarBodega(b);
 
         return m;
     }
 
 };
+
+template<class T>
+Matriz<T>::Matriz(){
+
+    arreglo = new T[MAX];
+    cont=0;
+    tam = MAX;
+}
+
+template<class T>
+Matriz<T>::~Matriz(){
+
+    delete[] arreglo;
+}
+
+template<class T>
+void Matriz<T>::expandir(){
+    T *nuevo = new T[tam+MAX];
+
+    for (size_t i=0; i<cont; i++){
+        nuevo[i]=arreglo[i];
+    }
+
+    delete[] arreglo;
+    arreglo = nuevo;
+    tam=tam+MAX;
+}
+
+template<class T>
+size_t Matriz<T>::size(){
+    return cont;
+}
+
+template<class T>
+void Matriz<T>::insertar_final(T &v){
+    if(cont == tam){
+        expandir();
+    }
+
+    arreglo[cont]=v;
+    cont++;
+}
+
+template<class T>
+void Matriz<T>::insertar_inicio(T &v){
+    if(cont == tam){
+        expandir();
+    }
+
+    for (size_t i =cont; i > 0; i--)
+    {
+        arreglo[i]=arreglo[i-1];
+    }
+    arreglo[0] = v;
+    cont++;
+    
+}
+
+template<class T>
+void Matriz<T>::mostrar(){
+    cout<<left;
+    cout<<setw(15)<<"Producto";
+    cout<<setw(10)<<"Cantidad";
+    cout<<setw(10)<<"Precio";
+    cout<<endl;
+    for(size_t i=0; i<cont; i++){
+        T & b = arreglo[i];
+        /*cout<<"Producto: "<<b.getProducto()<<endl;
+        cout<<"Cantidad: "<<b.getCantidad()<<endl;
+        cout<<"Precio: "<<b.getPrecio()<<endl;*/
+        cout<<b;
+        //cout<<endl;
+    }
+
+}
+
+
 
 #endif
