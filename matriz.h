1ffd617ee3254c32cfdc23cbb4511a4faa680aa0
LizetteMendoza <lizette.mendoza6892@alumnos.udg.mx>
2021-08-14 01:16:20 -0500

    Se agreraron las definiciones de los metodos de insercion al principio y al final

--- matriz.h
+++ matriz.h
@@ -4,15 +4,27 @@
 
 class Matriz{
 
-    Bodega arreglo[5];
+    Bodega *arreglo;
     size_t cont;
+    size_t tam;
+    const static size_t MAX = 5;
+
+    void expandir();
+
 
 public:
     Matriz();
+    ~Matriz();
     void agregarBodega(const Bodega&b);
     void mostrar();
     void respaldar();
     void recuperar();
+    void insertar_final(Bodega &v);
+    void insertar_inicio(Bodega &v);
+    size_t size();
+    Bodega operator[](size_t p){
+        return arreglo[p];
+    }
 
     friend Matriz& operator <<(Matriz& m, const Bodega&b){
         m.agregarBodega(b);
